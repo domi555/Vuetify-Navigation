de02e68b7be4b8b65f6b242fff2e3ee0dff33e9a
domi555 <66876360+domi555@users.noreply.github.com>
2021-09-19 11:43:36 +0200

    Update

---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-navigation-drawer class="green" app>
+      <span class="white--text title">v-navigation-drawer</span>
+    </v-navigation-drawer>
+    <v-app-bar class="yellow" app>
+      <span class="title">v-app-bar</span>
+    </v-app-bar>
+    <v-main class="blue">
+      <span class="white--text title">v-main</span>
+    </v-main>
+    <v-footer class="purple" app>
+      <span class="white--text title mx-auto">v-footer</span>
+    </v-footer>
+    <v-bottom-navigation class="cyan" app>
+      <span class="white--text title">v-bottom-navigation</span>
+    </v-bottom-navigation>
+
+    <!--(1)-->
+    <v-system-bar class="pink" app>
+      <span class="white--text title">v-system-bar</span>
+    </v-system-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
