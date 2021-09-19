bcf6c09f5e9e08697293110ed782d10281575453
domi555 <66876360+domi555@users.noreply.github.com>
2021-09-19 12:40:46 +0200

    Update: Aufgabe 2

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer class="green" app>
+    <!-- <v-navigation-drawer class="green" app>
       <span class="white--text title">v-navigation-drawer</span>
     </v-navigation-drawer>
     <v-app-bar class="yellow" app>
@@ -14,12 +14,39 @@
     </v-footer>
     <v-bottom-navigation class="cyan" app>
       <span class="white--text title">v-bottom-navigation</span>
-    </v-bottom-navigation>
+    </v-bottom-navigation> -->
 
     <!--(1)-->
-    <v-system-bar class="pink" app>
+    <!-- <v-system-bar class="pink" app>
       <span class="white--text title">v-system-bar</span>
-    </v-system-bar>
+    </v-system-bar> -->
+
+    <v-app-bar class="grey lighten-5" height="74" app>
+      <img
+        class="mx-2"
+        src="images/fearlessfido-logo.svg"
+        style="height:150px; margin-top:100px"
+      />
+      <span
+        class="text-truncate text-h4 blue-grey--text darken-1"
+        style="font-family: Poppins"
+        >A Dog With a Mission</span
+      >
+      <v-spacer></v-spacer>
+      <v-btn active-class="amber darken-2" to="/" exact>Home</v-btn>
+      <v-btn active-class="amber darken-2" to="/products" exact class="ml-5"
+        >Products</v-btn
+      >
+      <v-btn active-class="amber darken-2" to="/about" exact class="ml-5">About</v-btn>
+    </v-app-bar>
+    <v-main>
+      <div class="d-flex">
+        <div style="width:250px"></div>
+        <div class="mt-10" style="flex:1">
+          <router-view></router-view>
+        </div>
+      </div>
+    </v-main>
   </v-app>
 </template>
 
@@ -32,3 +59,10 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+a:not(active-class) {
+  color: #ffffff !important;
+  background-color: #333333 !important;
+}
+</style>
