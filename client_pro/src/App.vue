170c296fde836679ccb3f731b2d24b4809cabba9
domi555 <66876360+domi555@users.noreply.github.com>
2021-09-22 19:11:34 +0200

    Update

--- client_pro/src/App.vue
+++ client_pro/src/App.vue
@@ -1,26 +1,5 @@
 <template>
   <v-app>
-    <!-- <v-navigation-drawer class="green" app>
-      <span class="white--text title">v-navigation-drawer</span>
-    </v-navigation-drawer>
-    <v-app-bar class="yellow" app>
-      <span class="title">v-app-bar</span>
-    </v-app-bar>
-    <v-main class="blue">
-      <span class="white--text title">v-main</span>
-    </v-main>
-    <v-footer class="purple" app>
-      <span class="white--text title mx-auto">v-footer</span>
-    </v-footer>
-    <v-bottom-navigation class="cyan" app>
-      <span class="white--text title">v-bottom-navigation</span>
-    </v-bottom-navigation> -->
-
-    <!--(1)-->
-    <!-- <v-system-bar class="pink" app>
-      <span class="white--text title">v-system-bar</span>
-    </v-system-bar> -->
-
     <v-app-bar class="grey lighten-5" height="74" app>
       <img
         class="mx-2"
